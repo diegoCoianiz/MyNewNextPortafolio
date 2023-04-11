d48916df1a3857112643327eed69a84b1a6c2f0b
Juan Diego Coianiz <diego.c.coianiz@gmail.com>
2023-04-11 21:28:18 +0200

    modify: whatsapp num

--- src/pages/api/whatsapp_message/index.js
+++ src/pages/api/whatsapp_message/index.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
     if (req.method === 'POST') {
         const { body } = req;
         const textToWhatsapp = trasformBodyInToASCII(body)
-        res.status(200).json({ message: `https://api.whatsapp.com/send?phone=5493413751398&text=${textToWhatsapp}` });
+        res.status(200).json({ message: `https://api.whatsapp.com/send?phone=34603795321&text=${textToWhatsapp}` });
     } else {
         res.status(405).json({ message: 'Método no permitido.' });
     }
